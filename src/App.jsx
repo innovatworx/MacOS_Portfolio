1eb3c870ee982724f388efc9703f4fee512f4581
M <innovant_devr@outlook.com>
2026-04-08 19:33:55 -0500

    implement dock-part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from "#components/Navbar.jsx";
+import {Welcome} from "#components/index.js";
+import Dock from "#components/Dock.jsx";
 
 const App = () => {
     return (
         <main>
             <Navbar />
+            <Welcome />
+            <Dock />
         </main>
     );
 };
